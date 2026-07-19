4ccb3d0a4cabed8095ea801f3367b311bd741408
cae calcolari <eduardokae01@hotmail.com>
2026-07-19 10:56:27 -0300

    feat: finaliza o agente

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from graph.workflow import build_graph
+
+graph = build_graph()
+
+result = graph.invoke({
+    "file_path": "examples/diff.txt"
+})
+
+print()
+print("RESUMO")
+print(result["summary"])
+
+print()
+print("RECOMENDACAO")
+print(result["recommendation"])
+
+print()
+print("RELATORIO")
+print(result["report_path"])
